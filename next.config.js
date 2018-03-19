9212599543459392b8f51cd8e3a9096d18da521f
Steven Bell <steven@budgee.com>
2018-03-19 12:56:07 -0400

    remove preact

--- next.config.js
+++ next.config.js
@@ -17,11 +17,6 @@ module.exports = {
     config.node = {
       fs: 'empty',
     }
-    config.resolve.alias = {
-      react: 'preact-compat/dist/preact-compat',
-      'react-dom': 'preact-compat/dist/preact-compat',
-    }
-
     return config
   },
 }
